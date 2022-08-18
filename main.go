f1c3602b0ba82603f743ed795a0602194828cfcd
slashtechno <77907286+slashtechno@users.noreply.github.com>
2022-08-18 13:54:10 -0400

    Added ability to get blog ID from the Blogger API

--- main.go
+++ main.go
@@ -11,17 +11,17 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/tidwall/gjson"
+	"github.com/tidwall/gjson" 
 )
 
 type Configuration struct {
-	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 	ClientID     string `json:"client_id"`
 	ClientSecret string `json:"client_secret"`
 	// Scope             string   `json:"scope"`
 	AuthorizationCode string   `json:"authorization_code"`
-	Blogs             []string `json:"blogs"`
+	Blog             string `json:"blog"`
+	BlogID             string `json:"blog_id"`
 }
 
 var configuration Configuration
@@ -43,88 +43,101 @@ func main() {
 {
 	"client_id": "",
 	"client_secret": "",
-	"blogs": []
+	"blog": ""
 }
 		
 `)
 		fmt.Println(`Please add the appropriate values to config.json
 client_id and client_secret can be retrieved from Google Cloud Console
-blogs should be an array of blogs in the following format: [“example.com", “2.example.com”]`)
+blog should be your blog's URL. For example, https://example.blogspot.com`)
 		os.Exit(0)
 	}
 
 	configuration := loadConfiguration()
-	emptyCode := 0
+
+	message := "Please set the following in config.json"
 	if configuration.ClientID == "" {
-		emptyCode = 1
-	} else if configuration.ClientSecret == "" {
-		emptyCode = 2
-	} else if configuration.ClientSecret == "" && configuration.ClientID == "" {
-		emptyCode = 3
+		message += "\n- client_id"
+	} 
+	if configuration.ClientSecret == "" {
+		message += "\n- client_secret"
+	} 
+	if configuration.Blog == ""{
+		message += "\n- blog"
 	}
-	switch emptyCode {
-	case 1:
-		log.Fatalln("Please set client_id in config.json")
-	case 2:
-		log.Fatalln("Please set client_secret in config.json")
-	case 3:
-		log.Fatalln("Please set client_id and client_secret in config.json")
+	
+	if message != "Please set the following in config.json"{
+		log.Fatalln(message)
 	}
 	if configuration.RefreshToken == "" {
 		log.Println("No refresh token found")
 		getRefreshToken()
 	}
+	if configuration.BlogID == "" {
+		log.Println("No blog ID found")
+		getBlogID()
+	}
 
-	log.Println(getAccessToken())
-}
+	getAccessToken()}
 
 func getAccessToken() string {
 	// Get access token using the refresh token
 	url := "https://oauth2.googleapis.com/token?client_id=" + configuration.ClientID + "&client_secret=" + configuration.ClientSecret + "&refresh_token=" + configuration.RefreshToken + "&redirect_uri=https%3A%2F%2Foauthcodeviewer.netlify.app&grant_type=refresh_token"
-	// Send a POST request, to the URL above ^ and convert the result body to bytes, which is then converted to a string
-	req, err := http.NewRequest("POST", url, nil)
-	checkNilErr(err)
-	res, err := http.DefaultClient.Do(req)
-	checkNilErr(err)
-	defer res.Body.Close()
-	resultBodyBytes, err := io.ReadAll(res.Body)
-	checkNilErr(err)
-	resultBody := string(resultBodyBytes)
-	// Get the authorization token
-	configuration.AccessToken = gjson.Get(resultBody, "access_token").String()
-	return configuration.AccessToken
+	// Send a POST request to the URL with no authorization headers
+	resultBody := request(url, "POST", false)
+	// Get the authorization token	
+	accessToken := gjson.Get(resultBody, "access_token").String()
+	return accessToken
 }
 
 func getRefreshToken() {
-
+	// Get the authorization code from the user
 	fmt.Println("Please go to the following link in your browser:")
 	fmt.Println("\nhttps://accounts.google.com/o/oauth2/v2/auth?client_id=" + configuration.ClientID + "&redirect_uri=https%3A%2F%2Foauthcodeviewer.netlify.app&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fblogger&response_type=code&access_type=offline&prompt=consent\n")
 	fmt.Println("Input the authorization code below")
 	reader := bufio.NewReader(os.Stdin)
 	authorizationCode, err := reader.ReadString('\n')
+	fmt.Print("\n")
 	checkNilErr(err)
 	if strings.TrimSpace(authorizationCode) == "!exit!" {
 		os.Exit(0)
 	}
 	configuration.AuthorizationCode = strings.TrimSpace(authorizationCode)
-	// log.Println(configuration.AuthorizationCode)
 
 	// Get refresh token using the authorization code given by the user
 	url := "https://oauth2.googleapis.com/token?client_id=" + configuration.ClientID + "&client_secret=" + configuration.ClientSecret + "&code=" + configuration.AuthorizationCode + "&redirect_uri=https%3A%2F%2Foauthcodeviewer.netlify.app&grant_type=authorization_code"
-	// Send a POST request, to the URL above ^ and convert the result body to bytes, which is then converted to a string
-	req, err := http.NewRequest("POST", url, nil)
+	// Send a POST request to the URL with no authorization headers
+	resultBody := request(url, "POST", false)
+	configuration.RefreshToken = gjson.Get(resultBody, "refresh_token").String()
+	writeConfiguration()
+}
+
+
+func getBlogID() string{
+	url := "https://www.googleapis.com/blogger/v3/blogs/byurl?url=" + configuration.Blog+ "%2F"
+	// Send a GET request to the URL with bearer authorization
+	resultBody := request(url, "GET", true)
+	configuration.BlogID = gjson.Get(resultBody, "id").String()
+	writeConfiguration()
+	return configuration.BlogID
+}
+
+func request(url string, requestType string, bearerAuth bool) string{
+	// Send a request to the URL, with the URL which was passed to the function
+	req, err := http.NewRequest(requestType, url, nil)
 	checkNilErr(err)
+	// If the bearerAuth parameter is true, set the Authorization header with an access token
+	if bearerAuth{
+	req.Header.Add("Authorization", "Bearer " + getAccessToken())
+	}
+	// Make the actual request
 	res, err := http.DefaultClient.Do(req)
 	checkNilErr(err)
+	// Convert the result body to a string and then return it
 	defer res.Body.Close()
 	resultBodyBytes, err := io.ReadAll(res.Body)
 	checkNilErr(err)
-	resultBody := string(resultBodyBytes)
-
-	// Get the refresh token
-	configuration.RefreshToken = gjson.Get(resultBody, "refresh_token").String()
-	log.Println(configuration.RefreshToken) // Print refresh token as string
-	writeConfiguration()
+	return string(resultBodyBytes)
 }
 
 func checkNilErr(err any) {
