e15e6859256ea92feaa355631744e0b308810b3e
slashtechno <77907286+slashtechno@users.noreply.github.com>
2022-08-18 19:07:28 -0400

    Added ability to publish to Blogger

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module blogger-oauth
+module blogger-oauth-poster
 
 go 1.18
 
 require github.com/tidwall/gjson v1.14.3
 
 require (
+	github.com/gomarkdown/markdown v0.0.0-20220817224203-2206187d3406
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -11,7 +11,9 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/tidwall/gjson" 
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
+	"github.com/tidwall/gjson"
 )
 
 type Configuration struct {
@@ -19,9 +21,17 @@ type Configuration struct {
 	ClientID     string `json:"client_id"`
 	ClientSecret string `json:"client_secret"`
 	// Scope             string   `json:"scope"`
-	AuthorizationCode string   `json:"authorization_code"`
-	Blog             string `json:"blog"`
-	BlogID             string `json:"blog_id"`
+	AuthorizationCode string `json:"authorization_code"`
+	Blog              string `json:"blog"`
+	BlogID            string `json:"blog_id"`
+}
+type PostJson struct {
+	Kind string `json:"kind"`
+	Blog struct {
+		ID string `json:"id"`
+	} `json:"blog"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 var configuration Configuration
@@ -58,15 +68,15 @@ blog should be your blog's URL. For example, https://example.blogspot.com`)
 	message := "Please set the following in config.json"
 	if configuration.ClientID == "" {
 		message += "\n- client_id"
-	} 
+	}
 	if configuration.ClientSecret == "" {
 		message += "\n- client_secret"
-	} 
-	if configuration.Blog == ""{
+	}
+	if configuration.Blog == "" {
 		message += "\n- blog"
 	}
-	
-	if message != "Please set the following in config.json"{
+
+	if message != "Please set the following in config.json" {
 		log.Fatalln(message)
 	}
 	if configuration.RefreshToken == "" {
@@ -78,14 +88,83 @@ blog should be your blog's URL. For example, https://example.blogspot.com`)
 		getBlogID()
 	}
 
-	getAccessToken()}
+	genPost()
+}
+
+func genPost() {
+	fmt.Println(`Post formatting (input numeric selection):
+1) Raw HTML
+2) Markdown (will get converted to HTML automatically)`)
+	reader := bufio.NewReader(os.Stdin)
+	formatting, err := reader.ReadString('\n')
+	checkNilErr(err)
+	formatting = strings.TrimSpace(formatting)
+	fmt.Print("\n")
+	fmt.Print("Title: ")
+	title := singleLineInput()
+	if formatting == "1" {
+		fmt.Print("COMPLETE filepath to HTML file: ")
+		filepath := singleLineInput()
+		htmlBytes, err := os.ReadFile(filepath)
+		html := string(htmlBytes)
+		checkNilErr(err)
+		fmt.Print("Type \"Yes\" and press enter to confirm ")
+		confirmation := singleLineInput()
+		if confirmation == "Yes" {
+			pushPost(html, title)
+		} else {
+			log.Fatalln("Confirmation was not given")
+		}
+	} else if formatting == "2" {
+		fmt.Print("COMPLETE filepath to Markdown file: ")
+		filepath := singleLineInput()
+		markdownBytes, err := os.ReadFile(filepath)
+		checkNilErr(err)
+		// Setup markdown parser extensions
+		extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+		mdparser := parser.NewWithExtensions(extensions)
+		htmlBytes := markdown.ToHTML(markdownBytes, mdparser, nil)
+		fmt.Print("Type \"Yes\" and press enter to confirm ")
+		confirmation := singleLineInput()
+		if confirmation == "Yes" {
+			pushPost(string(htmlBytes), title)
+		} else {
+			log.Fatalln("Confirmation was not given")
+		}
+	} else {
+		log.Fatalln("Invalid option")
+	}
+
+}
+
+func pushPost(content string, title string) {
+	url := "https://www.googleapis.com/blogger/v3/blogs/" + configuration.BlogID + "/posts/"
+	payloadStruct := PostJson{Kind: "blogger#post", Blog: struct {
+		ID string `json:"id"`
+	}{ID: getBlogID()}, Title: title, Content: content}
+	payload, err := json.Marshal(payloadStruct)
+	checkNilErr(err)
+	req, err := http.NewRequest("POST", url, strings.NewReader(string(payload)))
+	checkNilErr(err)
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+getAccessToken())
+	_, err = http.DefaultClient.Do(req)
+	// res, err := http.DefaultClient.Do(req)
+	checkNilErr(err)
+	// defer res.Body.Close()
+	// resultBodyBytes, err := io.ReadAll(res.Body)
+	// checkNilErr(err)
+	// resultBody := string(resultBodyBytes)
+	// log.Println(resultBody)
+
+}
 
 func getAccessToken() string {
 	// Get access token using the refresh token
 	url := "https://oauth2.googleapis.com/token?client_id=" + configuration.ClientID + "&client_secret=" + configuration.ClientSecret + "&refresh_token=" + configuration.RefreshToken + "&redirect_uri=https%3A%2F%2Foauthcodeviewer.netlify.app&grant_type=refresh_token"
 	// Send a POST request to the URL with no authorization headers
 	resultBody := request(url, "POST", false)
-	// Get the authorization token	
+	// Get the authorization token
 	accessToken := gjson.Get(resultBody, "access_token").String()
 	return accessToken
 }
@@ -95,14 +174,7 @@ func getRefreshToken() {
 	fmt.Println("Please go to the following link in your browser:")
 	fmt.Println("\nhttps://accounts.google.com/o/oauth2/v2/auth?client_id=" + configuration.ClientID + "&redirect_uri=https%3A%2F%2Foauthcodeviewer.netlify.app&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fblogger&response_type=code&access_type=offline&prompt=consent\n")
 	fmt.Println("Input the authorization code below")
-	reader := bufio.NewReader(os.Stdin)
-	authorizationCode, err := reader.ReadString('\n')
-	fmt.Print("\n")
-	checkNilErr(err)
-	if strings.TrimSpace(authorizationCode) == "!exit!" {
-		os.Exit(0)
-	}
-	configuration.AuthorizationCode = strings.TrimSpace(authorizationCode)
+	configuration.AuthorizationCode = singleLineInput()
 
 	// Get refresh token using the authorization code given by the user
 	url := "https://oauth2.googleapis.com/token?client_id=" + configuration.ClientID + "&client_secret=" + configuration.ClientSecret + "&code=" + configuration.AuthorizationCode + "&redirect_uri=https%3A%2F%2Foauthcodeviewer.netlify.app&grant_type=authorization_code"
@@ -112,9 +184,8 @@ func getRefreshToken() {
 	writeConfiguration()
 }
 
-
-func getBlogID() string{
-	url := "https://www.googleapis.com/blogger/v3/blogs/byurl?url=" + configuration.Blog+ "%2F"
+func getBlogID() string {
+	url := "https://www.googleapis.com/blogger/v3/blogs/byurl?url=" + configuration.Blog + "%2F"
 	// Send a GET request to the URL with bearer authorization
 	resultBody := request(url, "GET", true)
 	configuration.BlogID = gjson.Get(resultBody, "id").String()
@@ -122,13 +193,13 @@ func getBlogID() string{
 	return configuration.BlogID
 }
 
-func request(url string, requestType string, bearerAuth bool) string{
+func request(url string, requestType string, bearerAuth bool) string {
 	// Send a request to the URL, with the URL which was passed to the function
 	req, err := http.NewRequest(requestType, url, nil)
 	checkNilErr(err)
 	// If the bearerAuth parameter is true, set the Authorization header with an access token
-	if bearerAuth{
-	req.Header.Add("Authorization", "Bearer " + getAccessToken())
+	if bearerAuth {
+		req.Header.Add("Authorization", "Bearer "+getAccessToken())
 	}
 	// Make the actual request
 	res, err := http.DefaultClient.Do(req)
@@ -147,12 +218,19 @@ func checkNilErr(err any) {
 	}
 }
 
+func singleLineInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	checkNilErr(err)
+	input = strings.TrimSpace(input)
+	fmt.Print("\n")
+	return input
+}
+
 func loadConfiguration() Configuration {
 	configFile, err := os.OpenFile(configPath, os.O_RDONLY, 0644)
 	checkNilErr(err)
 	json.NewDecoder(configFile).Decode(&configuration)
-	if configuration.RefreshToken == "" {
-	}
 	return configuration
 }
 
