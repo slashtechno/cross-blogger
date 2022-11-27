96d2972fd9c6f5674be9257413f678d4d618cd9a
slashtechno <77907286+slashtechno@users.noreply.github.com>
2022-11-27 15:44:45 -0600

    Added ping command for leaning Cobra Following https://youtu.be/SSRIn5DAmyw?t=1273

--- cmd/net/ping.go
+++ cmd/net/ping.go
@@ -2,28 +2,54 @@ package net
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
+var (
+	urlPath string
+	client  = http.Client{
+		Timeout: time.Second * 10,
+	}
+)
+
+func ping(url string) (int, error) {
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	return resp.StatusCode, nil
+}
+
 // pingCmd represents the ping command
 var pingCmd = &cobra.Command{
 	Use:   "ping",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "This pings a remote URL and returns the response",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ping called")
+		if resp, err := ping(urlPath); err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println(resp)
+		}
 	},
 }
 
 func init() {
 
 	// Here you will define your flags and configuration settings.
+	pingCmd.Flags().StringVarP(&urlPath, "url", "u", "", "URL to ping")
+
+	if err := pingCmd.MarkFlagRequired("url"); err != nil {
+		// If url is not provided, then print the error
+		fmt.Println(err.Error())
+	}
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
