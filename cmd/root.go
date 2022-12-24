c32e854c1a0a239573d12e3505a56e6c206ce7a7
slashtechno <77907286+slashtechno@users.noreply.github.com>
2022-12-24 15:44:17 -0500

    Reinitalized with Viper

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,18 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package cmd
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/slashtechno/cross-blogger/cmd/net"
-	"github.com/slashtechno/cross-blogger/cmd/info"
-
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -33,21 +38,40 @@ func Execute() {
 	}
 }
 
-func addSubcommandPalettes() {
-	rootCmd.AddCommand(net.NetCmd)
-	rootCmd.AddCommand(info.InfoCmd)
-}
-
 func init() {
+	cobra.OnInitialize(initConfig)
+
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cross-blogger.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cross-blogger.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
 
-	addSubcommandPalettes()
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".cross-blogger" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".cross-blogger")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 slashtechno
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 
 */
 package main
