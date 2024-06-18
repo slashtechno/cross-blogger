a556fb83c196fc9ba8ae9ebe5bfd17bba3283e59
Angad Behl <77907286+slashtechno@users.noreply.github.com>
2024-06-18 00:55:57 +0000

    feat: Allow for multiple destinations and attributes
    Might just be better to use a file via viper

--- cobra/cmd/publish.go
+++ cobra/cmd/publish.go
@@ -1,45 +1,65 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
-	"fmt"
+	"strings"
 
+	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 )
 
-// publishCmd represents the publish command
 var publishCmd = &cobra.Command{
 	Use:   "publish",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.OnlyValidArgs,
+	Short: "Publish to a destination",
+	Long:  `Publish to a destination. Available destinations: blogger, markdown, html. Make sure to specify with <platform>,<key1>=<value1>,<key2>=<value2>,...`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("publish called")
+		destinations, _ := cmd.Flags().GetStringSlice("destination")
+		// Make a map of platforms and their attributes
+		platformMap := make(map[string]map[string]string)
+
+		// Iterate over the destinations
+		for _, destination := range destinations {
+			// Split the destination into platform and attributes
+			// This splits the destination into two parts: the platform and the attributes
+			// Anything after the first semicolon is considered attributes
+			destinationParts := strings.SplitN(destination, ";", 2)
+			platform := destinationParts[0]
+			// Create a new map for the platform if it doesn't exist
+			if _, ok := platformMap[platform]; !ok {
+				platformMap[platform] = make(map[string]string)
+			}
+			// Check if there are any attributes
+			if len(destinationParts) > 1 {
+				// Split the attributes part on semicolon
+				attributes := strings.Split(destinationParts[1], ";")
+				// Populate the map with the attributes
+				for _, attribute := range attributes {
+					keyValue := strings.Split(attribute, "=")
+					if len(keyValue) == 2 {
+						platformMap[platform][keyValue[0]] = keyValue[1]
+					}
+				}
+			}
+		}
+
+		// Debug log the destination map
+		log.Debugf("Platform map: %v", platformMap)
+		// Iterate over the platforms
+		for platform, attributes := range platformMap {
+			// Debug log the platform and attributes
+			log.Debugf("Platform: %s, Attributes: %v", platform, attributes)
+		}
+
 	},
 }
 
 func init() {
+	// example command: go run . publish --destination "blogger;blogAddress=example.com;postAddress=example-post" --destination "markdown;filepath=example.md" --title "Example Title" --dry-run
 	rootCmd.AddCommand(publishCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// publishCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-
-	// Google OAuth flags
+	publishCmd.Flags().StringSliceP("destination", "d", nil, "Destination(s) to publish to\nAvailable destinations: blogger, markdown, html\nMake sure to specify with <platform>,<key1>=<value1>,<key2>=<value2>,...")
+	publishCmd.Flags().StringP("title", "t", "", "Specify custom title instead of using the default")
+	publishCmd.Flags().BoolP("dry-run", "r", false, "Don't actually publish")
 	publishCmd.Flags().String("client-id", "", "Google OAuth client ID")
 	publishCmd.Flags().String("client-secret", "", "Google OAuth client secret")
 	publishCmd.Flags().String("refresh-token", "", "Google OAuth refresh token")
-
 }
--- go.mod
+++ go.mod
@@ -5,19 +5,30 @@ go 1.19
 require (
 	github.com/JohannesKaufmann/html-to-markdown v1.4.0
 	github.com/alexflint/go-arg v1.4.3
+	github.com/charmbracelet/log v0.4.0
 	github.com/gomarkdown/markdown v0.0.0-20221013030248-663e2500819c
 	github.com/imdario/mergo v0.3.16
 	github.com/sirupsen/logrus v1.9.2
 	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
+	github.com/spf13/cobra v1.8.1
 	github.com/tidwall/gjson v1.16.0
 )
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 )
