87a5ecf16bdebb9b357db922b0ae4cfa65bbb522
Angad Behl <77907286+slashtechno@users.noreply.github.com>
2024-06-25 13:57:36 +0000

    fix: run TrimSpace on generated post description

--- internal/platforms/blogger.go
+++ internal/platforms/blogger.go
@@ -157,7 +157,7 @@ func (b Blogger) Pull(options PushPullOptions) (PostData, error) {
 		if err != nil {
 			return PostData{}, err
 		}
-		// TODO: It seems that sometimes, when writing to markdown, the description is written with extra whitespace
+		postDescription = strings.TrimSpace(postDescription)
 		log.Debug("Generated description", "description", postDescription)
 	}
 
